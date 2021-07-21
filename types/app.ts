46690a2bce84bcc8388d5ee1e59a6ebee529c889
Joshua Wilson <wilson.jd022@gmail.com>
2021-07-21 16:06:07 -0700

    added small type file =

---
+++ types/app.ts
@@ -0,0 +1,17 @@
+/*
+  A short program that runs over why types are important
+  TLDR: Types are everywhere and they can help immensely
+  with autocompletion and error checking.
+*/
+
+const today = new Date();
+today.getDate();
+
+const person = {
+  age: 20,
+};
+person.age;
+person.happy;
+
+class Color {}
+const red = new Color();
