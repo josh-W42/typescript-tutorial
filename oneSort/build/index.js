308095f646f8f50f84762c55587df8e8a824c1a5
Joshua Wilson <wilson.jd022@gmail.com>
2021-07-23 07:56:08 -0700

    finnished the 'bad' solution for the sorter

--- oneSort/build/index.js
+++ oneSort/build/index.js
@@ -26,6 +26,10 @@
 
 */
 var Sorter = /** @class */ (function () {
+    // bad solution
+    // if we try to sort for a number or a string, we could use
+    // the pipe operator to say collection is either an array of
+    // numbers or a string..
     function Sorter(collection) {
         this.collection = collection;
     }
@@ -34,11 +38,23 @@ var Sorter = /** @class */ (function () {
         var length = this.collection.length;
         for (var i = 0; i < length; i++) {
             for (var j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    var leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftHand;
+                // bad solution for sorting multiple types
+                // if collection is an array of numbers - use this:
+                // using a "type guard"
+                // we use type guards when we want TS to restore the functionality
+                // of a type when the type could be ambiguous (currently number[] | string)
+                if (this.collection instanceof Array) {
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        var leftHand = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = leftHand;
+                    }
                 }
+                // if collection is a string - use this:
+                if (this.collection instanceof String) {
+                    // logic for swapping characters
+                }
+                // if collection is a.... etc
             }
         }
     };
--- oneSort/src/index.ts
+++ oneSort/src/index.ts
@@ -26,7 +26,11 @@
 */
 
 class Sorter {
-  constructor(public collection: number[]) {}
+  // bad solution
+  // if we try to sort for a number or a string, we could use
+  // the pipe operator to say collection is either an array of
+  // numbers or a string..
+  constructor(public collection: number[] | string) {}
 
   sort(): void {
     // We're just using bubble sort, nothing fancy
@@ -34,11 +38,26 @@ class Sorter {
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHand;
+        // bad solution for sorting multiple types
+
+        // if collection is an array of numbers - use this:
+        // using a "type guard"
+        // we use type guards when we want TS to restore the functionality
+        // of a type when the type could be ambiguous (currently number[] | string)
+        if (this.collection instanceof Array) {
+          if (this.collection[j] > this.collection[j + 1]) {
+            const leftHand = this.collection[j];
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = leftHand;
+          }
         }
+
+        // if collection is a string - use this:
+        if (this.collection instanceof String) {
+          // logic for swapping characters
+        }
+
+        // if collection is a.... etc
       }
     }
   }
