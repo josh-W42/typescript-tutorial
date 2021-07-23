5a1482f2986fc63f01b14afefb0f5de12d30bfb9
Joshua Wilson <wilson.jd022@gmail.com>
2021-07-22 20:28:28 -0700

    initalized project

---
+++ maps/src/index.ts
@@ -0,0 +1,5 @@
+// if you need to remember to use parcel
+// just type parcel index.html at the maps directory
+// parcel just allows you to run TS in the browser.
+// had to install parcel globally with npm
+console.log("ello");
