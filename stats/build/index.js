a58f8e179b707ce01d084d9e1d944574017373da
Joshua Wilson <wilson.jd022@gmail.com>
2021-07-23 13:00:09 -0700

    added some general notes about the project

--- stats/build/index.js
+++ stats/build/index.js
@@ -6,6 +6,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var CsvFileReader_1 = require("./CsvFileReader");
 var MatchReader_1 = require("./MatchReader");
 var MatchResult_1 = require("./MatchResult");
+/*
+  This project is large and can be confusing to look through but
+  what we're trying to accomplish here is 2 different paths to make
+  this code reuseable,
+
+  We tried an inheritance pathway that follows a 'is a' pattern
+  between classes
+
+  Then we tried a composition pathway that follows a 'has a' pattern
+  between classes
+
+  The point that we attempted to make was that sometimes composition can
+  give rise to more reusable code
+*/
 // for the interface path
 // create an object that satisfies the 'dataReader' interface
 var csvFileReader = new CsvFileReader_1.CsvFileReader("football.csv");
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -5,6 +5,21 @@ import { CsvFileReader } from "./CsvFileReader";
 import { MatchReader } from "./MatchReader";
 import { MatchResult } from "./MatchResult";
 
+/*
+  This project is large and can be confusing to look through but
+  what we're trying to accomplish here is 2 different paths to make
+  this code reuseable,
+
+  We tried an inheritance pathway that follows a 'is a' pattern
+  between classes
+
+  Then we tried a composition pathway that follows a 'has a' pattern
+  between classes
+
+  The point that we attempted to make was that sometimes composition can
+  give rise to more reusable code
+*/
+
 // for the interface path
 
 // create an object that satisfies the 'dataReader' interface
